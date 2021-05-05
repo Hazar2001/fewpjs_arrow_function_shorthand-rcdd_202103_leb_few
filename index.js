5ad2dbafd65573ccbcdd5503a4506a6098a32511
Hazar Saad ElDine <saadeldinehazar@gmail.com>
2021-05-05 09:21:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 function divide(){
   return 2000/100;
 }
+<<<<<<< HEAD
 let square = x => x*x;
 let add=(parameter1, parameter2) => parameter1 + parameter2
+=======
+let divide =x => x*x;
+>>>>>>> 71764073b30bcaf0bf016a4f3135c275a301f039
